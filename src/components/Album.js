48e8ff6709904d76b98e957bac6910cd0e296bfe
Morgan Holland <morgan.holland@gmail.com>
2018-04-08 08:36:45 -0700

    added play, pause, setsong, and handlesongclick methods

--- src/components/Album.js
+++ src/components/Album.js
@@ -11,7 +11,36 @@ class Album extends Component {
 
     this.state = {
       album: album,
+      currentSong: album.songs[0],
+      isPlaying: false
     };
+    this.audioElement = document.createElement('audio');
+    this.audioElement.src = album.songs[0].audioSrc;
+  }
+
+  play() {
+    this.audioElement.play();
+    this.setState({ isPlaying: true });
+  }
+
+  pause() {
+    this.audioElement.pause();
+    this.setState({ isPlaying: false });
+  }
+
+  setSong(song) {
+    this.audioElement.src = song.audioSrc;
+    this.setState({ currentSong:song });
+  }
+
+  handleSongClick(song) {
+    const isSameSong = this.state.currentSong === song;
+    if (this.state.isPlaying && isSameSong) {
+      this.pause();
+    } else {
+      if (!isSameSong) { this.setSong(song); }
+      this.play();
+    }
   }
 
   render() {
@@ -32,19 +61,21 @@ class Album extends Component {
              <col id="song-duration-column" />
            </colgroup>
            <tbody>
-
              {
                this.state.album.songs.map( (song, index) =>
-                <section className="song">
-                 <tr>
-                   <td>{index + 1}</td>
-                   <td>{song.title}</td>
-                   <td>{song.duration}</td>
+                 <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
+                   <td className="song-actions">
+                     <button>
+                       <span className="song-number">{index + 1}</span>
+                       <span className="ion-play"></span>
+                       <span className="ion-pause"></span>
+                     </button>
+                   </td>
+                   <td className="song-title">{song.title}</td>
+                   <td className="song-duration">{song.duration}</td>
                  </tr>
-                </section>
                )
              }
-
            </tbody>
          </table>
       </section>
