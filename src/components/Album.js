077490319b70d700f46965108d703ea3ee6a99c0
Morgan Holland <morgan.holland@gmail.com>
2018-04-08 10:27:37 -0700

    added next button functionality

--- src/components/Album.js
+++ src/components/Album.js
@@ -52,6 +52,14 @@ class Album extends Component {
     this.play(newSong);
   }
 
+  handleNextClick() {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = Math.min(this.state.album.songs.length - 1, currentIndex + 1);
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play(newSong);
+  }
+
   render() {
     return (
       <section className="album">
@@ -92,6 +100,7 @@ class Album extends Component {
            currentSong={this.state.currentSong}
            handleSongClick={() => this.handleSongClick(this.state.currentSong)}
            handlePrevClick={() => this.handlePrevClick()}
+           handleNextClick={() => this.handleNextClick()}
          />
       </section>
     );
